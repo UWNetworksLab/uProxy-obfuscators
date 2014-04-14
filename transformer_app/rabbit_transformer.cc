500e05a7854116f271f33d53ba9e4a633709299b
shanjian <quantsword@gmail.com>
2014-04-14 12:14:05 -0700

    transformer api implementation using pNaCl.

---
+++ transformer_app/rabbit_transformer.cc
@@ -0,0 +1,49 @@
+#include "rabbit_transformer.h"
+#include <stdlib.h>
+
+using namespace std;
+
+bool RabbitTransformer::SetKey(const uint8_t* key_str, uint32_t key_len) {
+  if (key_len != cryptor_.kDefaultKeySize) {
+    return false;
+  }
+  cryptor_.keysetup(key_str, cryptor_.kDefaultKeySize, cryptor_.kDefaultIvSize);
+  return true;
+}
+
+bool RabbitTransformer::Transform(
+    const uint8_t* data, uint32_t data_len,
+    vector<string>& transformed_data) {
+
+  uint8_t iv[cryptor_.kDefaultIvSize];
+
+  // TODO(shanjian) this is a only a placeholder for nouce generation.
+  for (int i = 0; i < cryptor_.kDefaultIvSize; ++i) {
+      iv[i] = rand() & 0xff;
+  }
+  cryptor_.ivsetup(iv); 
+
+  uint32_t buf_len = cryptor_.kDefaultIvSize + data_len;
+  string result;
+  result.resize(buf_len);
+  char* buf_ptr = const_cast<char*>(result.data());
+  memcpy(buf_ptr, iv, cryptor_.kDefaultIvSize);
+  buf_ptr += cryptor_.kDefaultIvSize;
+  cryptor_.encrypt_bytes(data, reinterpret_cast<uint8_t*>(buf_ptr), data_len);
+  transformed_data.clear();
+  transformed_data.push_back(result);
+  return true;
+}
+
+bool RabbitTransformer::Restore(const uint8_t* data, uint32_t data_len,
+                                std::string& result) {
+  const uint8_t* iv = data;
+  cryptor_.ivsetup(iv); 
+
+  result.resize(data_len - cryptor_.kDefaultIvSize);
+  uint8_t* buf = reinterpret_cast<uint8_t*>(const_cast<char*>(result.data()));
+  cryptor_.decrypt_bytes(data + cryptor_.kDefaultIvSize,
+                         buf, data_len - cryptor_.kDefaultIvSize);
+  return true;
+}
+
---
+++ transformer_app/rabbit_transformer.h
@@ -0,0 +1,37 @@
+#ifndef __RABBIT_TRANSFORMER_H__
+#define __RABBIT_TRANSFORMER_H__
+
+#include "transformer.h"
+#include "rabbit.h"
+
+class RabbitTransformer : public Transformer {
+public:
+  RabbitTransformer() {};
+
+  virtual bool SetKey(const uint8_t* key_str, uint32_t key_len);
+
+  virtual bool Configure(const uint8_t* config_data,
+                         uint32_t config_data_len) { 
+    return true; 
+  }
+
+  virtual bool Transform(const uint8_t* data, uint32_t data_len,
+                         std::vector<std::string>& transformed_data);
+  virtual bool FlushTransform(std::vector<std::string>& transformed_data) {
+    return true;
+  }
+
+  virtual bool Restore(const uint8_t* data, uint32_t data_len,
+                       std::string& result);
+
+  virtual TransformerError GetErrorInfo(std::string* detail = NULL) {
+    return eTransformNoneError;
+  }
+
+protected:
+  RabbitCryptor cryptor_;
+};
+
+#endif // __RABBIT_TRANSFORMER_H__
+
+
