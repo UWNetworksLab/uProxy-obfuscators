8126997b2e6d81738243e703600e2867beab283f
Kevin P. Dyer <kpdyer@gmail.com>
2014-05-15 18:16:13 -0700

    ugly, but working, async. FTE tests

---
+++ transformer_fte/html/js/benchmark.data.js
@@ -0,0 +1,42 @@
+      var testResults = [
+          {
+              'inputLanguage': '^\C+$',
+              'inputMaxLen': 128,
+              'outputLanguage': '^\C+$',
+              'outputMaxLen': 128,
+              'key': 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+              'plaintext': 'a'
+          },
+          {
+              'inputLanguage': '^\C+$',
+              'inputMaxLen': 128,
+              'outputLanguage': '^\C+$',
+              'outputMaxLen': 128,
+              'key': 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+              'plaintext': 'a'
+          },
+          {
+              'inputLanguage': '^\C+$',
+              'inputMaxLen': 128,
+              'outputLanguage': '^\C+$',
+              'outputMaxLen': 128,
+              'key': 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+              'plaintext': 'a'
+          },
+          {
+              'inputLanguage': '^\C+$',
+              'inputMaxLen': 128,
+              'outputLanguage': '^\C+$',
+              'outputMaxLen': 128,
+              'key': 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+              'plaintext': 'a'
+          },
+          {
+              'inputLanguage': '^\C+$',
+              'inputMaxLen': 128,
+              'outputLanguage': '^\C+$',
+              'outputMaxLen': 128,
+              'key': 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+              'plaintext': 'a'
+          }
+      ];
