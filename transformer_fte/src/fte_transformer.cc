c5448bb06ad1847c3bf2cf0bd441f2ad0af744ac
Kevin P. Dyer <kpdyer@gmail.com>
2014-05-15 21:09:50 -0700

    code cleanup

--- transformer_fte/src/fte_transformer.cc
+++ transformer_fte/src/fte_transformer.cc
@@ -2,32 +2,31 @@
 
 #include "fte_transformer.h"
 
-FteTransformer::FteTransformer()
-{
+FteTransformer::FteTransformer() {
 
 }
 
 bool FteTransformer::Transform(
     const uint8_t* data, uint32_t data_len,
     std::vector<std::string>& transformed_data) {
-    
+
     const char * s = reinterpret_cast<const char *>(data);
     std::string datagram(s, data_len);
     std::string ciphertext = cryptor_.encrypt(datagram);
     transformed_data.push_back(ciphertext);
-    
+
     return true;
 }
 
 
 bool FteTransformer::Restore(const uint8_t* data, uint32_t data_len,
-                                std::string& result) {
-    
+                             std::string& result) {
+
     const char * s = reinterpret_cast<const char *>(data);
     std::string datagram(s, data_len);
     std::string plaintext = cryptor_.decrypt(datagram);
     result = plaintext;
-    
+
     return true;
 }
 
@@ -47,6 +46,6 @@ bool FteTransformer::Configure(const uint8_t* data, uint32_t data_len) {
     cryptor_ = fte::FTE(VALID_DFA_5,128,
                         VALID_DFA_5,128,
                         "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
-    
+
     return true;
 }
--- transformer_fte/src/fte_transformer.h
+++ transformer_fte/src/fte_transformer.h
@@ -10,34 +10,34 @@
 
 
 class FteTransformer : public Transformer {
-public:
-  FteTransformer();
-  
-  virtual ~FteTransformer() {};
+  public:
+    FteTransformer();
 
-  virtual bool SetKey(const uint8_t* key_str, uint32_t key_len);
+    virtual ~FteTransformer() {};
 
-  virtual bool SetInitVector(const uint8_t* data, uint32_t data_len);
+    virtual bool SetKey(const uint8_t* key_str, uint32_t key_len);
 
-  virtual bool Configure(const uint8_t* config_data,
-                         uint32_t config_data_len);
+    virtual bool SetInitVector(const uint8_t* data, uint32_t data_len);
 
-  virtual bool Transform(const uint8_t* data, uint32_t data_len,
-                         std::vector<std::string>& transformed_data);
-  
-  virtual bool FlushTransform(std::vector<std::string>& transformed_data);
+    virtual bool Configure(const uint8_t* config_data,
+                           uint32_t config_data_len);
 
-  virtual bool Restore(const uint8_t* data, uint32_t data_len,
-                       std::string& result);
+    virtual bool Transform(const uint8_t* data, uint32_t data_len,
+                           std::vector<std::string>& transformed_data);
 
-  virtual TransformerError GetErrorInfo(std::string* detail = NULL) {
-    return eTransformNoneError;
-  }
+    virtual bool FlushTransform(std::vector<std::string>& transformed_data);
 
-protected:
-  fte::FTE     cryptor_;
-  std::string  init_vector_;
-  std::string  key_;
+    virtual bool Restore(const uint8_t* data, uint32_t data_len,
+                         std::string& result);
+
+    virtual TransformerError GetErrorInfo(std::string* detail = NULL) {
+        return eTransformNoneError;
+    }
+
+  protected:
+    fte::FTE     cryptor_;
+    std::string  init_vector_;
+    std::string  key_;
 };
 
 #endif // __FTE_TRANSFORMER_H__
