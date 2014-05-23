251d6fb8617e7b5e5a17e8bbbf79599f7e74005a
Kevin P. Dyer <kpdyer@gmail.com>
2014-05-23 09:59:56 -0700

    build process update

--- transformer_fte/vagrant32/build.sh
+++ transformer_fte/vagrant32/build.sh
@@ -11,15 +11,18 @@ export LLVM=$BUILD_DIR/emscripten-fastcomp/Release/bin
 export GIT_EMSCRIPTEN=https://github.com/kripken/emscripten.git
 export GIT_EMSCRIPTEN_FASTCOMP=https://github.com/kripken/emscripten-fastcomp
 export GIT_EMSCRIPTEN_FASTCOMP_CLANG=https://github.com/kripken/emscripten-fastcomp-clang
+
 export HTTP_GMP=https://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.bz2
+
 export GIT_LIBFTE=https://github.com/uProxy/libfte.git
 export GIT_OBFUSCATION=https://github.com/uProxy/obfuscation.git
 
 export EMCC_CORES=`nproc`
 
 # gtest relies on cxxabi, so we need to include this following hack
-export CFLAGS="-O3"
-export CXXFLAGS="-O3 -I$BUILD_DIR/emscripten/system/lib/libcxxabi/include"
+export CFLAGS="-O3 -I$INSTALL_DIR/include"
+export CXXFLAGS="-O3 -I$INSTALL_DIR/include -I$BUILD_DIR/emscripten/system/lib/libcxxabi/include"
+export LDFLAGS="-O3 -L$INSTALL_DIR/lib"
 
 ###
 mkdir -p $BUILD_DIR
@@ -33,7 +36,7 @@ git clone $GIT_EMSCRIPTEN_FASTCOMP
 cd emscripten-fastcomp/tools
 git clone $GIT_EMSCRIPTEN_FASTCOMP_CLANG clang
 cd ..
-./configure --enable-optimized --disable-assertions --enable-targets=host,js
+./configure --prefix=$INSTALL_DIR --enable-optimized --disable-assertions --enable-targets=host,js
 make -j`nproc`
 
 
@@ -43,8 +46,8 @@ $BUILD_DIR/emscripten/emcc
 
 # fix broken emar
 #  - using emar provided by emscripten results in archives that cause llvm-nm to hang
-sudo rm /vagrant/sandbox/emscripten/emar
-sudo ln -s /usr/bin/ar /vagrant/sandbox/emscripten/emar
+sudo rm $BUILD_DIR/emscripten/emar
+sudo ln -s /usr/bin/ar $BUILD_DIR/emscripten/emar
 
 
 # build/install gmp
@@ -56,15 +59,15 @@ emconfigure ./configure --prefix=$INSTALL_DIR --disable-assembly --enable-shared
 # hack
 sed -i 's/HAVE_OBSTACK_VPRINTF 1/HAVE_OBSTACK_VPRINTF 0/g' config.h
 make -j`nproc`
-cp -f gmpxx.h $INSTALL_DIR/include/
 make install
+cp -f gmpxx.h $INSTALL_DIR/include/
 
 
 # build libfte
 cd $BUILD_DIR
 git clone $GIT_LIBFTE
 cd libfte
-emconfigure ./configure --prefix=$INSTALL_DIR --with-gmp=$INSTALL_DIR
+emconfigure ./configure --prefix=$INSTALL_DIR
 make -j`nproc`
 make install
 
@@ -73,5 +76,7 @@ make install
 cd $BUILD_DIR
 git clone $GIT_OBFUSCATION
 cd obfuscation/transformer_fte
-emconfigure ./configure --prefix=$INSTALL_DIR --with-libfte=$INSTALL_DIR --with-gmp=$INSTALL_DIR
+emconfigure ./configure
 make -j`nproc`
+cp -f fte.js /vagrant/
+cp -fr html /vagrant/
