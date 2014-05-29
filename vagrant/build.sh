8b5e164bf9763ff3b732d627804ed15d1bd6883c
Kevin P. Dyer <kpdyer@gmail.com>
2014-05-28 18:00:52 -0700

    build script improvements

--- vagrant/build.sh
+++ vagrant/build.sh
@@ -5,26 +5,38 @@
 export WORKING_DIR=/home/vagrant
 export BUILD_DIR=$WORKING_DIR/build
 export INSTALL_DIR=$WORKING_DIR/install
-export PATH=$BUILD_DIR/emscripten-fastcomp/Release/bin:$BUILD_DIR/emscripten:$PATH
+export PATH=$INSTALL_DIR/bin:$BUILD_DIR/emscripten-fastcomp/Release/bin:$BUILD_DIR/emscripten:$PATH
 export LLVM=$BUILD_DIR/emscripten-fastcomp/Release/bin
 
 export GIT_EMSCRIPTEN=https://github.com/kripken/emscripten.git
 export GIT_EMSCRIPTEN_FASTCOMP=https://github.com/kripken/emscripten-fastcomp
 export GIT_EMSCRIPTEN_FASTCOMP_CLANG=https://github.com/kripken/emscripten-fastcomp-clang
 export HTTP_GMP=https://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.bz2
+export HTTP_NODEJS=http://nodejs.org/dist/v0.10.28/node-v0.10.28.tar.gz
 export GIT_LIBFTE=https://github.com/uProxy/libfte.git
 export GIT_OBFUSCATION=https://github.com/uProxy/obfuscation.git
 
 export EMCC_CORES=`nproc`
 
 # gtest relies on cxxabi, so we need to include this following hack
-export CFLAGS="-O3 -I$INSTALL_DIR/include"
+export CFLAGS="-O3"
 export CXXFLAGS="-O3 -I$INSTALL_DIR/include -I$BUILD_DIR/emscripten/system/lib/libcxxabi/include"
 export LDFLAGS="-O3 -L$INSTALL_DIR/lib"
 
 ###
 mkdir -p $BUILD_DIR
 mkdir -p $INSTALL_DIR
+
+
+# install nodejs
+cd $BUILD_DIR
+wget $HTTP_NODEJS
+tar xvf node-v0.10.28.tar.gz
+cd node-*
+./configure --prefix=$INSTALL_DIR
+make -j`nproc`
+make install
+ln -s $INSTALL_DIR/bin/node $INSTALL_DIR/bin/nodejs
 
 
 # https://github.com/kripken/emscripten/wiki/LLVM-Backend
@@ -76,5 +88,11 @@ git clone $GIT_OBFUSCATION
 cd obfuscation/transformer_fte
 emconfigure ./configure
 make -j`nproc`
+
+# test the transformers
+nodejs bin/test.fte.js
+nodejs bin/test.rabbit.js
+
+# copy to shared directroy
 mkdir -p /vagrant/build
-cp -fr html /vagrant/build
+cp -fr html /vagrant/build/
