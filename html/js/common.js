07c1f061274c900b7c94986ee417f824000e3aad
shanjian <quantsword@gmail.com>
2014-06-05 10:43:53 -0700

    fix the test by changing arraybuffer conversion to use uint8, which limits to ascii only support.

--- html/js/common.js
+++ html/js/common.js
@@ -1,13 +1,16 @@
 // Utility function to convert ArrayBuffer to string.
+// The function has limitation, it won't convert non-ascii utf8 or
+// utf16 binary representation to readable string. 
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 
 // Utility function to convert string to ArrayBuffer.
+// This function can only deal with ascii character.
 function str2ab(str) {
   var buf = new Uint8Array(str.length * 2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf.buffer);
-  for (var i = 0, strLen = str.length; i < strLen; i++) {
+  var bufView = new Uint8Array(buf.buffer);
+  for (var i = 0; i < str.length; i++) {
     bufView[i] = str.charCodeAt(i);
   }
   return buf.buffer;
