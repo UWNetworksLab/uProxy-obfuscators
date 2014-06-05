64603bd3d95cec3d128f8fa18e8ba5ed504d4411
shanjian <quantsword@gmail.com>
2014-06-04 17:57:41 -0700

    change str2ab/ab2str to really access ArrayBuffer instead typed array.

--- html/js/common.js
+++ html/js/common.js
@@ -1,16 +1,16 @@
-// Utility function to convert Uint8Array to string.
+// Utility function to convert ArrayBuffer to string.
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf.buffer));
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 
-// Utility function to convert string to Uint8Array.
+// Utility function to convert string to ArrayBuffer.
 function str2ab(str) {
   var buf = new Uint8Array(str.length * 2); // 2 bytes for each char
   var bufView = new Uint16Array(buf.buffer);
   for (var i = 0, strLen = str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
-  return buf;
+  return buf.buffer;
 }
 
 function str2ab8(str) {
